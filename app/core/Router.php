c123eb275b63ef8c8bfef9718c246035b6f36572
Luis Henrique <sathorofire2019@gmail.com>
2025-02-09 21:49:24 -0300

    modified:   app/core/Router.php

--- app/core/Router.php
+++ app/core/Router.php
@@ -14,7 +14,7 @@ class Router
      * 
      * @param string $uri URI da página
      * @param string $request Tipo de requisição (*GET*, *POST*, *PUT*, *DELETE*)
-     * @param string $controller Controlador e método a executar { '*Controller:method*' }
+     * @param string $controller Controlador e método a executar { '*Controller::method*' }
      */
     public function add(string $uri, string $request, string $controller): void
     {
